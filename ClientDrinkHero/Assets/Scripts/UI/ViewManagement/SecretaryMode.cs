e138a75609c0696555fb023bf339e2a60ec472d3
BloodyJason <bloodynoobontour@web.de>
2023-08-09 15:30:29 +0200

    secretaries + background + scriptable objects + chibi animation import + secretary mode fix

--- ClientDrinkHero/Assets/Scripts/UI/ViewManagement/SecretaryMode.cs
+++ ClientDrinkHero/Assets/Scripts/UI/ViewManagement/SecretaryMode.cs
@@ -7,7 +7,6 @@ using UnityEngine.UI;
 
 public class SecretaryMode : MonoBehaviour
 {
-    [SerializeField] private List<Hero> unlockedHeroes = new List<Hero>();
     [SerializeField] private GameObject dialogTextBox;
     [SerializeField] private GameObject buttons;
     [SerializeField] private TextMeshProUGUI dialogText;
@@ -18,20 +17,11 @@ public class SecretaryMode : MonoBehaviour
     private float textBoxTimer;
     private int lastQuoteIndex = -1;
 
-    private UnlockedHeroesPreviewHandler _unlockedHeroesPreviewHandler;
-
     void Start()
     {
         buttons.SetActive(false);
         DisableTextBox();
 
-        foreach (Hero hero in HeroHolder.Instance.Heroes)
-        {
-            if (hero.Unlocked)
-            {
-                unlockedHeroes.Add(hero);
-            }
-        }
         LoadSecretary(currentSelectionIndex);
 
         buttons.SetActive(true);
@@ -41,36 +31,36 @@ public class SecretaryMode : MonoBehaviour
     {
         currentSelectionIndex += increment;
         if (currentSelectionIndex < 0)
-            currentSelectionIndex = unlockedHeroes.Count - 1;
-        if (currentSelectionIndex >= unlockedHeroes.Count)
+            currentSelectionIndex = HeroHolder.Instance.Heroes.Count - 1;
+        if (currentSelectionIndex >= HeroHolder.Instance.Heroes.Count)
             currentSelectionIndex = 0;
 
         if (currentSecretary)
             Destroy(currentSecretary);
 
-        currentSecretary = Instantiate(unlockedHeroes[currentSelectionIndex].SecretaryPrefab, secretaryHolder);
+        currentSecretary = Instantiate(HeroHolder.Instance.Heroes[currentSelectionIndex].SecretaryPrefab, secretaryHolder);
         currentSecretary.GetComponent<Button>().onClick.AddListener(Touch);
         currentSecretary.transform.GetChild(0).GetComponent<Button>().onClick.AddListener(TouchSpecial);
         DisableTextBox();
     }
     public void Touch()
     {
-        int randomQuoteIndex = Random.Range(0, unlockedHeroes[currentSelectionIndex].SecretaryQuotes.Count);
+        int randomQuoteIndex = Random.Range(0, HeroHolder.Instance.Heroes[currentSelectionIndex].SecretaryQuotes.Count);
         if (lastQuoteIndex == randomQuoteIndex)
         {
             randomQuoteIndex = randomQuoteIndex == 0 ? ++randomQuoteIndex : --randomQuoteIndex;
         }
         lastQuoteIndex = randomQuoteIndex;
 
-        SetText(unlockedHeroes[currentSelectionIndex].SecretaryQuotes[randomQuoteIndex]);
-        string heroName = unlockedHeroes[currentSelectionIndex].Name;
+        SetText(HeroHolder.Instance.Heroes[currentSelectionIndex].SecretaryQuotes[randomQuoteIndex]);
+        string heroName = HeroHolder.Instance.Heroes[currentSelectionIndex].Name;
         currentSecretary.GetComponentInChildren<Image>().sprite = AssetLoader.Instance.BorrowSprite("Assets/Art/Character/" + heroName + "/"  + heroName + "_" + randomQuoteIndex + ".png");
     }
 
     public void TouchSpecial()
     {
-        currentSecretary.GetComponentInChildren<Image>().sprite = unlockedHeroes[currentSelectionIndex].SecretarySpezialTouchImage;
-        SetText(unlockedHeroes[currentSelectionIndex].SecretarySpezialTouchQuote);
+        currentSecretary.GetComponentInChildren<Image>().sprite = HeroHolder.Instance.Heroes[currentSelectionIndex].SecretarySpezialTouchImage;
+        SetText(HeroHolder.Instance.Heroes[currentSelectionIndex].SecretarySpezialTouchQuote);
         lastQuoteIndex = -1;
     }
 
